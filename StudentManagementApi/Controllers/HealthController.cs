6bb566652a475fb2835e35a047139e02ac58687c
GEORGEfx <georgeebisike@gmail.com>
2025-08-06 02:56:55 +0100

    added health check controller

---
+++ StudentManagementApi/Controllers/HealthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentManagementApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("")]
+        public async Task<IActionResult> HealthCheck() => Ok("APi is running");
+    }
+}
